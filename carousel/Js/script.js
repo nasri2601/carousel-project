1318cc2a3cec19a3eae22148bf713cc2202f15ac
nasri samy <nasri.samy.dev@gmail.com>
2019-09-29 17:28:38 +0200

    add fuction scroll next

--- carousel/Js/script.js
+++ carousel/Js/script.js
@@ -4,3 +4,14 @@ const previous = document.querySelector('.previous')
 const images = document.querySelector('li')
 const button = document.querySelector('button')
 let i= 0
+
+
+function scrollingNext() {
+     images[i].classList.remove('active');
+     i++;
+     if (i>=document.images.length){
+          i = 0 
+
+     }
+     images[i].classList.add('active')
+}

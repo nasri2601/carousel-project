d64c7c2ee9bcdc6b6267daccfbd9898d87f844ef
nasri samy <nasri.samy.dev@gmail.com>
2019-09-29 18:12:46 +0200

    test change

--- Js/script.js
+++ Js/script.js
@@ -1,30 +1,28 @@
-'USE STRICT'
-const next = document.querySelector('.next')
-const previous = document.querySelector('.previous')
-const images = document.querySelector('li')
-const button = document.querySelector('button')
-let i= 0
+'use script';
 
 
+
+const next = document.querySelector('.next');
+const previous = document.querySelector('.previous');
+const images = document.querySelectorAll('li');
+const button = document.querySelector('button');
+let i = 0;
+​
+​
 function scrollingNext() {
-     images[i].classList.remove('active');
-     i++;
-     if (i>=document.images.length){
-          i = 0 
-
-     }
-     images[i].classList.add('active')
+   images[i].classList.remove('active'); i++;
+    if (i>= document.images.length) {
+         i = 0
+        }
+   images[i].classList.add('active');
 }
-
-function scrollingPrevious() {
-     images[i].classList.remove('active');
-     i--;
-     if (i<0){
-          i = images.length -1 
-
-     }
-     images[i].classList.add('active')
-}
-
-next.addEventListener('click' , scrollingNext)
-previous.addEventListener('click' , scrollingPrevious)
+​
+ function scrollingPrevious() {
+    images[i].classList.remove('active'); i--;
+    if (i<0) {
+         i = images.length -1
+        }
+   images[i].classList.add('active');
+ }
+next.addEventListener('click',scrollingNext);
+previous.addEventListener('click',scrollingPrevious);

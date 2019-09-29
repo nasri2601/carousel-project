e3d80c36c9ca18542ef4302d217f7a935531a7be
nasri samy <nasri.samy.dev@gmail.com>
2019-09-29 17:24:41 +0200

    add variable for carousel

--- carousel/Js/script.js
+++ carousel/Js/script.js
@@ -1,2 +1,6 @@
-const images = document.querySelectorAll('li');
-const button = document.querySelector('button');
+'USE STRICT'
+const next = document.querySelector('.next')
+const previous = document.querySelector('.previous')
+const images = document.querySelector('li')
+const button = document.querySelector('button')
+let i= 0
